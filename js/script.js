677958cc7dcc05284d1ac9140d864c164fe3e7c4
riccardo-guido <guidoriccardo02@gmail.com>
2025-04-01 14:41:24 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,7 @@
+const userTrip = parseInt(prompt("Quanti km devi percorrere?"));
+const userAge = parseInt(prompt("Quanti anni hai?"));
+
+const ticketPrice = userTrip * 0.21;
+
+console.log(ticketPrice);
+
